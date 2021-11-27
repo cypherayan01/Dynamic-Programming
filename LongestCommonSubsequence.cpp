afffd49fe8c5ce2951103109b1c6199096d19529
cypherayan01 <86187011+cypherayan01@users.noreply.github.com>
2021-11-27 18:18:24 +0530

    Add files via upload

---
+++ LongestCommonSubsequence.cpp
@@ -0,0 +1,28 @@
+int longestCommonSubsequence(string s1, string s2) {
+        int x=s1.size(),y=s2.size();
+        int t[x+1][y+1];
+        for(int i=0;i<=x;i++)
+        {
+            for(int j=0;j<=y;j++)
+            {
+                if(i==0||j==0)
+                {
+                    t[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<=x;i++)
+        {
+            for(int j=1;j<=y;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[x][y];
+    }
